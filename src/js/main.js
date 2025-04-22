202dbec3d89a2818e8089c69c51f3749b05efa07
Vahan92 <vahan.kalenteryan.dev@gmail.com>
2025-04-23 00:12:01 +0400

    Make bold active link

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,17 @@ function renderRoute() {
   document.getElementById("home").classList.toggle("hidden", isAnalytics);
   document.getElementById("analytics").classList.toggle("hidden", !isAnalytics);
 
+  const homeLink = document.querySelector('a[href="#/"]');
+  const analyticsLink = document.querySelector('a[href="#/analytics"]');
+
+  if (isAnalytics) {
+    homeLink.classList.remove("font-bold");
+    analyticsLink.classList.add("font-bold");
+  } else {
+    homeLink.classList.add("font-bold");
+    analyticsLink.classList.remove("font-bold");
+  }
+
   if (!isAnalytics && !homeInitialized) {
     initHome();
     homeInitialized = true;
